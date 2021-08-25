be54b26932a473035eff65da99b7ad9847602ec9
Mark Fullmer <jfullmer@austin.utexas.edu>
2021-08-25 12:24:51 -0500

    20 - Display past events (#141)
    * Allow configurable option to display past events (Resolves #20)
    
    * Re-add newline before class end
    
    * Use removeHandler instead of setHandler

---
+++ modules/utevent_view_listing_page/src/Routing/RouteSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\utevent_view_listing_page\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+use Drupal\Core\Routing\RoutingEvents;
+
+/**
+ * Listens to the dynamic route events.
+ *
+ * @package Drupal\utevent_view_listing_page\Routing
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    // Ensure our route alterations occur last.
+    $events[RoutingEvents::ALTER] = ['onAlterRoutes', -9999];
+    return $events;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    $config = \Drupal::config('utevent_view_listing_page.config');
+    if ($config->get('display_past_events')) {
+      return;
+    }
+    if ($route = $collection->get('view.utevent_listing_page.past')) {
+      $route->setRequirement('_access', 'FALSE');
+    }
+  }
+
+}
