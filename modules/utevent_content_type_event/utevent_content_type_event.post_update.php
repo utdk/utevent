58c450b68305d2310a80f3fd44df637f1c9458d5
Jeff Cardwell <jacardwell@utexas.edu>
2023-02-10 09:00:13 -0600

    244 - Drupal 10 compatibility (Part 1: nominal code compatibility) (#251)
    * Update core/core_version_requirement
    
    * Fix deprecations
    
    * Update addtocal_augment

--- modules/utevent_content_type_event/utevent_content_type_event.post_update.php
+++ modules/utevent_content_type_event/utevent_content_type_event.post_update.php
@@ -11,17 +11,20 @@ use Symfony\Component\Yaml\Yaml;
  * Configure XMLSitemap settings.
  */
 function utevent_content_type_event_post_update_configure_xmlsitemap() {
+  /** @var \Drupal\Core\Extension\ExtensionPathResolver $extension_path_resolver */
+  $extension_path_resolver = \Drupal::service('extension.path.resolver');
   if (\Drupal::moduleHandler()->moduleExists('xmlsitemap') !== FALSE) {
     if (\Drupal::config('xmlsitemap.settings.node.utevent_event')->get('status') === NULL) {
       $config = \Drupal::configFactory()->getEditable('xmlsitemap.settings.node.utevent_event');
-      $config_path = drupal_get_path('module', 'utevent_content_type_event') . '/config/install/xmlsitemap.settings.node.utevent_event.yml';
+      $config_path = $extension_path_resolver->getPath('module', 'utevent_content_type_event') . '/config/install/xmlsitemap.settings.node.utevent_event.yml';
       if (!empty($config_path)) {
         $data = Yaml::parse(file_get_contents($config_path));
         if (is_array($data)) {
           $config->setData($data)->save(TRUE);
         }
       }
-    } else {
+    }
+    else {
       $message = dt('XML Sitemap configuration object "xmlsitemap.settings.node.utevent_event" found. No action taken.');
       \Drupal::messenger()->addMessage($message);
       \Drupal::logger('utexas')->notice($message);
