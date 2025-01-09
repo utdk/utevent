a676e507f863b0c570b7338bf43f94bb9324e6a1
jmf3658 <jfullmer@austin.utexas.edu>
2025-01-09 07:00:13 -0700

    Switch email to survey link

--- modules/utevent_content_type_event/src/Form/ContentConfig.php
+++ modules/utevent_content_type_event/src/Form/ContentConfig.php
@@ -21,11 +21,11 @@ class ContentConfig extends BaseConfigurationForm {
       '#type' => 'textfield',
       '#placeholder' => 'e.g., "Dell Medical School", "Department of Student Affairs"',
       '#default_value' => $default_department ?? '',
-      '#description' => $this->t('Upcoming events are available for display on <a href=":link">:link</a>. Specify a department name to be associated with this site\'s events. <ul><li>The name entered must match an existing department name listed <a href=":departments">here</a>.</li><li>If there is no appropriate department, email <a href="mailto:events">:events</a>.</li><li>To have your events listed on calendar.utexas.edu, email <a href="mailto:events">:events</a>, providing the link to the site event feed,  <code>:feed</code></li></ul>', [
+      '#description' => $this->t('Upcoming events are available for display on <a href=":link">:link</a>. Specify a department name to be associated with this site\'s events. <ul><li>The name entered must match an existing department name listed <a href=":departments">here</a>.</li><li>If there is no appropriate department, use the <a href=":survey">calendar.utexas.edu support form</a>.</li><li>To have your events listed on calendar.utexas.edu, use the <a href=":survey">calendar.utexas.edu support form</a>, providing the link to the site event feed, <code>:feed</code></li></ul>', [
         ':feed' => \Drupal::request()->getSchemeAndHttpHost() . '/events/localist.csv',
         ':link' => 'https://calendar.utexas.edu',
         ':departments' => 'https://docs.google.com/spreadsheets/d/1HBoBuMMNjgkJYOm7fI4CpYNrHpbSL61vJMEvXTG7DIY/edit?gid=31309768#gid=31309768',
-        ':events' => 'universityevents@austin.utexas.edu',
+        ':survey' => 'https://utexas.qualtrics.com/jfe/form/SV_0jNfR9GXvYv69V4',
       ]),
     ];
     $form['#submit'][] = [$this, 'submitContentConfig'];
